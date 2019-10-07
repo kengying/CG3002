cf136ed53134ad904052383c6f8e671ae6bdfa64
kengying <L.kengying@gmail.com>
2019-10-07 10:39:59 +0800

    checksum + integration

--- Comms/Arduino/chcksum/chcksum.ino
+++ Comms/Arduino/chcksum/chcksum.ino
@@ -2,8 +2,24 @@
 #include <semphr.h> 
 #define STACK_SIZE    200
 
+#include <Wire.h>
+
+const int MPU1 = 0x68, MPU2 = 0x69; // MPU6050 I2C addresses
+const int SEL_2G_200D = 0x0, SEL_4G_500D = 0x8, SEL_8G_1000D = 0x10, SEL_16G_2000D = 0x18;
+const float CONV_2G = 16384.0, CONV_4G = 8192.0, CONV_8G = 4096.0, CONV_16G = 2048.0;
+const float CONV_200D = 131.0, CONV_500D = 65.5, CONV_1000D = 32.8, CONV_2000D = 16.4;
+
 // Create Sempaphore
 SemaphoreHandle_t xSemaphore;
+
+struct SensorData {
+  float accX;  // Accelerometer x-axis
+  float accY;  // Accelerometer y-axis
+  float accZ;  // Accelerometer z-axis
+  float gX; // Gyroscope x-axis
+  float gY; // Gyroscope y-axis
+  float gZ; // Gyroscope z-axis
+} MPU1Data, MPU2Data;
 
 typedef struct DataPacket{
   int sensor1ID;
@@ -30,11 +46,43 @@ int checksum2;
 char checksum_c[4];
 
 void setup() {
-  Serial.begin(115200);
-  Serial1.begin(115200);
+  Wire.begin();                 // Initiates I2C communication
+  Wire.beginTransmission(MPU1); // Slave address
+  Wire.write(0x6B);             // Access the power management register
+  Wire.write(0x00);             // Wakes up the MPU
+  Wire.endTransmission(true);
+  
+  Wire.beginTransmission(MPU1); // Slave address
+  Wire.write(0x1B);             // Access Gyroscope Scale Register
+  Wire.write(SEL_8G_1000D);     // Set Scale to +-1000deg/s
+  Wire.endTransmission(true);   // Communication done
+
+  Wire.beginTransmission(MPU1); // Slave address
+  Wire.write(0x1C);             // Access Accelerometer Scale Register
+  Wire.write(SEL_4G_500D);      // Set Scale to +-4g
+  Wire.endTransmission(true);   // Communication done
+  
+  Wire.begin();                 // Initiates I2C communication
+  Wire.beginTransmission(MPU2); // Slave address
+  Wire.write(0x6B);             // Access the power management register
+  Wire.write(0x00);             // Wakes up the MPU
+  Wire.endTransmission(true);   // Communication done
+
+  Wire.beginTransmission(MPU2); // Slave address
+  Wire.write(0x1B);             // Access Gyroscope Scale Register
+  Wire.write(SEL_8G_1000D);     // Set Scale to +-1000deg/s
+  Wire.endTransmission(true);   // Communication done
+
+  Wire.beginTransmission(MPU2); // Slave address
+  Wire.write(0x1C);             // Access Accelerometer Scale Register
+  Wire.write(SEL_4G_500D);      // Set Scale to +-4g
+  Wire.endTransmission(true);   // Communication done
+  
+  Serial.begin(9600);
+  Serial1.begin(9600);
   Serial.println("SET UP");
   
-  startHandshake();
+  //startHandshake();
   
   xSemaphore = xSemaphoreCreateBinary();
   if ( ( xSemaphore ) != NULL ) {
@@ -86,13 +134,51 @@ void initRun(void *p){
     
     Serial.println();
     // 30 ms interval, ~30 samples/s
-    vTaskDelayUntil(&xCurrWakeTime, 30/portTICK_PERIOD_MS);
+    vTaskDelayUntil(&xCurrWakeTime, 300/portTICK_PERIOD_MS);
   }
 }
+
+SensorData getDataFromMPU(const int MPU, SensorData datain) {
+    Serial.println("Get Data From MPU Run");
+
+  Wire.beginTransmission(MPU);      // Begin communication
+  Wire.write(0x3B);                 // Register 0x3B (ACCEL_XOUT_H) 
+  Wire.endTransmission(false);      // End communication
+  Wire.requestFrom(MPU, 14, true);  // Request 14 registers
+
+  // Combines the upper 8 bits and the lower 8 bits to form 16 bits
+  datain.accX  = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  datain.accY  = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  datain.accZ  = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L) To clear Temp from buffer.
+  datain.gX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  datain.gY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  datain.gZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  datain = translateValues(datain);
+  
+  return datain;
+}
+
+SensorData translateValues(SensorData dataTranslate) {
+  Serial.println("Translate Run");
+  dataTranslate.accX = dataTranslate.accX/CONV_4G;
+  dataTranslate.accY = dataTranslate.accY/CONV_4G;
+  dataTranslate.accZ = dataTranslate.accZ/CONV_4G;
+  dataTranslate.gX = dataTranslate.gX/CONV_1000D;
+  dataTranslate.gY = dataTranslate.gY/CONV_1000D;
+  dataTranslate.gZ = dataTranslate.gZ/CONV_1000D;
+
+  return dataTranslate;
+//  data.Temp = (data.Temp/340.00) + 36.53;
+}
+
 /**
  *  Read Value
  */
 void sensorValues() {
+    Serial.println("Read Sensor 1 Run");
+      MPU1Data = getDataFromMPU(MPU1, MPU1Data);
       /** 
        * Hand Values 
        * Accelerometer 
@@ -101,12 +187,16 @@ void sensorValues() {
        * -> x: 3, y: 4, z: 5
        */
       data.sensor1ID = 1;
-      data.sensor1[0] = 1; 
-      data.sensor1[1] = 2;
-      data.sensor1[2] = 3;
-      data.sensor1[3] = 4;
-      data.sensor1[4] = 5.1;
-      data.sensor1[5] = 6.1;
+      data.sensor1[0] = MPU1Data.accX; 
+      data.sensor1[1] = MPU1Data.accY;
+      data.sensor1[2] = MPU1Data.accZ;
+      data.sensor1[3] = MPU1Data.gX;
+      data.sensor1[4] = MPU1Data.gY;
+      data.sensor1[5] = MPU1Data.gZ;
+      
+        Serial.println("Read Sensor 2 Run");
+
+      MPU2Data = getDataFromMPU(MPU2, MPU2Data);
       /** 
        * Elbow Values
        * Accelerometer 
@@ -115,12 +205,22 @@ void sensorValues() {
        * -> x: 3, y: 4, z: 5
        */
       data.sensor2ID = 2;
-      data.sensor2[0] = -1;
-      data.sensor2[1] = -2;
-      data.sensor2[2] = -3;
-      data.sensor2[3] = -4;
-      data.sensor2[4] = -5;
-      data.sensor2[5] = -6;
+      data.sensor2[0] = MPU2Data.accX; 
+      data.sensor2[1] = MPU2Data.accY;
+      data.sensor2[2] = MPU2Data.accZ;
+      data.sensor2[3] = MPU2Data.gX;
+      data.sensor2[4] = MPU2Data.gY;
+      data.sensor2[5] = MPU2Data.gZ;
+
+      Serial.print(MPU1Data.accX); Serial.print(","); Serial.print(MPU1Data.accY); Serial.print(","); 
+  Serial.print(MPU1Data.accZ); Serial.print(","); Serial.print(MPU1Data.gX); Serial.print(",");
+  Serial.print(MPU1Data.gY); Serial.print(",");  Serial.print(MPU1Data.gZ); Serial.print(",");
+
+      Serial.print(MPU2Data.accX); Serial.print(","); Serial.print(MPU2Data.accY); Serial.print(","); 
+  Serial.print(MPU2Data.accZ); Serial.print(","); Serial.print(MPU2Data.gX); Serial.print(",");
+  Serial.print(MPU2Data.gY); Serial.print(",");  Serial.print(MPU2Data.gZ);
+
+  Serial.println();
       
 }
 
