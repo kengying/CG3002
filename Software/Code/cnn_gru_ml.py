3910c2791f7c1dcc6d48699648bc7889d358e54c
minato <dmc3hpcity@gmail.com>
2019-11-01 12:05:18 +0800

    Add cnn pre-trained model

---
+++ Software/Code/cnn_gru_ml.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from tensorflow.keras.models import load_model
+
+def cnn_load():
+    model = load_model('cnn_model.h5')
+    return model
+
+def cnn_predict(model, x_test):
+    y_pred = model.predict_classes(np.array([x_test]), batch_size=32, verbose=0)
+    return y_pred
